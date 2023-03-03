420166ac20b7abfab9cd500ab8a8bda982435941
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-03-03 15:27:17 +0000

    Fri, Mar 3, 2023, 3:27 PM +00:00

--- discount2.c
+++ discount2.c
@@ -1,2 +1,17 @@
 #include <cs50.h>
-#
+#include <stdio.h>
+
+float discount(float price, int percentage);
+
+int main (void)
+{
+    float regular = get_float("Regular Price: ");
+    int percent_off = get_int("percent off: ");
+    float sale = discount(regular, percent_off);
+    printf("sale price: %.2f" , sale);
+    }
+
+    float discount(float price, int percentage)
+    {
+        return price * (100-percentage) / 100
+    }
