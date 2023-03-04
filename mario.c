7ff9e8290cfdbf84159ddb3a3405ab3b357c633c
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-03-04 15:11:21 +0000

    Sat, Mar 4, 2023, 3:11 PM +00:00

--- mario.c
+++ mario.c
@@ -10,12 +10,17 @@ int n;
     }
     while (n<1);
 
+    // for each row
     for (int i=0; i<n; i++)
-    {
+
+            //for each column
             for (int s=0; s<n; s++)
             {
+                \\print a brick
                 printf("#");
             }
+            // move to next row
+            printf("\n");
     }
 
 }
