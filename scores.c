70cc1020c6fcaa992ca71002b74cffe1c0bb1fd0
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-03-25 13:57:38 +0000

    Sat, Mar 25, 2023, 1:57 PM +00:00

--- scores.c
+++ scores.c
@@ -4,9 +4,10 @@
 int main(void)
 {
     int scores [3];
-    scores [0]= get_int ("Score: ");
-    scores [1]= get_int ("Score: ");
-    scores [2]= get_int ("Score: ");
+    for (int i = 0; i<3; i++)
+    {
+        scores[i]= get_int("score: ");
+    }
 
     printf("Average: %f\n", (scores[0] +scores[1] +scores[2]) /3.0);
 }
