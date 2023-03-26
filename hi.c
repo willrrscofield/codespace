1c2c8a2793fc992fef6be954485c8aff552b68d5
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-03-26 21:19:25 +0000

    Sun, Mar 26, 2023, 9:19 PM +00:00

--- hi.c
+++ hi.c
@@ -2,9 +2,7 @@
 #include <cs50.h>
 int main (void)
 {
-    char c1='H';
-    char c2='I';
-    char c3='!';
+    string s="HI!";
 
-    printf("%i %i %i\n", c1, c2, c3);
+    printf("%i %i %i %i\n", s[0], s[1],s[2],s[3]);
     }
