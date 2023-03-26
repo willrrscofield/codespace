072c8c292ae4abcaaec9dd7b6afbf1e4d20b0e03
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-03-26 21:46:48 +0000

    Sun, Mar 26, 2023, 9:46 PM +00:00

--- length.c
+++ length.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 [
     string name = get_string("what's your name? ");
 
-    int n =0;
+    int n =strlen(name);
+    printf("%i\n", n);
 ]
