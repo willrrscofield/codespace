d591311c944af959bbb585992383a4f590425708
willrrscofield <77061394+willrrscofield@users.noreply.github.com>
2023-05-02 20:02:21 +0000

    Tue, May 2, 2023, 8:02 PM +00:00

--- uppercase.c
+++ uppercase.c
@@ -7,9 +7,9 @@ int main (void)
 {
     string s = get_string ("Before: ");
     printf("After: ");
-    for (int i =0, i < strlen(s); i++)
+    for (int i =0; i < strlen(s); i++)
     {
         printf("%c", toupper(s[i]));
     }
-    printf("\n);
+    printf("\n");
 }
